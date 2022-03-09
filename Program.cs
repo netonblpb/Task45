6488a8b0f819f96860146c6da6ddab6a10f94d92
netonblpb <skullbog@gmail.com>
2022-03-09 14:23:47 +0300

    Task45 Complete

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать числа Фибоначчи.
+
+Console.Clear();
+
+Console.Write("Сколько чисел показать? ");
+int count = int.Parse(Console.ReadLine());
+int fur = 0;
+int fur2 = 1;
+int change = 0;
+for (int i = 0; i < count; i++)
+{    
+    Console.Write(fur + fur2 + "   ");
+    change = fur2;
+    fur2 = fur + fur2;
+    fur = change;
+}
