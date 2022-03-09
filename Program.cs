748089593653f6e06855d4bdcb998765caf51cc7
netonblpb <skullbog@gmail.com>
2022-03-09 14:44:09 +0300

    Task47 Functional Fixed

--- Program.cs
+++ Program.cs
@@ -6,11 +6,18 @@ Console.Write("Сколько чисел показать? ");
 int count = int.Parse(Console.ReadLine());
 int fur = 0;
 int fur2 = 1;
-int change = 0;
-for (int i = 0; i < count; i++)
-{    
-    Console.Write(fur + fur2 + "   ");
-    change = fur2;
-    fur2 = fur + fur2;
-    fur = change;
+
+
+void Change(int x, int y, int z)
+{
+    int change = 0;
+    for (int i = 0; i < z; i++)
+    {
+        Console.Write(x + y + "   ");
+        change = y;
+        y = x + y;
+        x = change;
+    }
 }
+
+Change(fur, fur2, count);
